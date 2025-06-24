2fd0cdbce652b3ed89510dc811efb3b9040b9ec8
Ferdinand Engländer <ferdinand@creanimo.net>
2025-06-24 22:08:42 +0200

    fix mason setup call

--- lua/ferdi/plugins/mason.lua
+++ lua/ferdi/plugins/mason.lua
@@ -25,11 +25,6 @@ function M.config()
 	require("lsp-zero").setup()
 	require("mason").setup()
 
-	require("mason-lspconfig").setup_handlers {
-		function (server_name)
-		    require "lspconfig" [server_name].setup {}
-		end
-	}
 
 	require("mason-lspconfig").setup {
       ensure_installed = M.servers,
